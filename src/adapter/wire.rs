d1e89aebdb61387b421fd6c940519876fca1c41a
Rick Glenn <therickglenn@gmail.com>
2026-05-23 05:53:17 -0700

    fix: mark temporary adapter wire enum non-exhaustive

--- src/adapter/wire.rs
+++ src/adapter/wire.rs
@@ -1,5 +1,6 @@
 //! Wire message placeholders for the simulation adapter scaffold.
 
 /// Placeholder threshold wire message type for adapter import tests.
+#[non_exhaustive]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum PqcThresholdWireMsg {}
